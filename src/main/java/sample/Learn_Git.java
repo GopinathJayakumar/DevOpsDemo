26fb18e0fa561452c3c1092b632d32b43dfd5417
GopinathJayakumar <gopirandom@gmail.com>
2019-07-11 12:35:01 +0530

    "I updated another syso"

--- src/main/java/sample/Learn_Git.java
+++ src/main/java/sample/Learn_Git.java
@@ -3,6 +3,7 @@ package sample;
 public class Learn_Git {
 	public static void main(String[] args) {
 		System.out.println("Welcome to DevOps");
+		System.out.println("____________________");
 	}
 
 }
